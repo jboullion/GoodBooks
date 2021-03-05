7b3b23a4115c86cca54bdffed4e48573726dcc5c
jboullion <jboullion83@gmail.com>
2021-03-04 20:11:40 -0600

    Initial commit

---
+++ GoodBooks.Services/BookService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GoodBooks.Data;
+using GoodBooks.Data.Models;
+
+namespace GoodBooks.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly GoodBooksDbContext _db;
+        
+        public BookService(GoodBooksDbContext db)
+        {
+            _db = db;
+        }
+        
+        public List<Book> GetAllBooks()
+        {
+            return _db.Books.ToList();
+        }
+
+        public Book GetBook(int bookId)
+        {
+            //var book = _db.Books.First(book => book.Id == bookId);
+            //var book = _db.Books.FirstOrDefault(book => book.Id == bookId);
+            return _db.Books.Find(bookId);
+        }
+
+        public void AddBook(Book book)
+        {
+            _db.Add(book);
+            _db.SaveChanges();
+        }
+
+        public void DeleteBook(int bookId)
+        {
+            var bookToDelete = _db.Books.Find(bookId);
+            
+            if (bookToDelete != null)
+            {
+                _db.Remove(bookToDelete);
+            }
+
+            throw new InvalidOperationException("Can't delete book that doesn't exist");
+        }
+    }
+}
---
+++ GoodBooks.Web/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GoodBooks.Web.Controllers
+{
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/api/books")]
+        public ActionResult GetBooks()
+        {
+            return Ok("Books!");
+        }
+    }
+}

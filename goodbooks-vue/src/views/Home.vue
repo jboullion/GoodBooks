de4721acd070fd78b9892db358d7be299e5c59cd
jboullion <jboullion83@gmail.com>
2021-03-05 15:20:48 -0600

    Vue Installed and Book Service setup

---
+++ goodbooks-vue/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img id="logo" alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="GoodBooks 4 Sale"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+#logo {
+  max-width: 100%;
+}
+</style>
